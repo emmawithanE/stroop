21f24971ed8c57b2fcf5ff587f17edb8863e6028
emmawithanE <emma@catgirl.software>
2022-01-03 13:13:48 +1030

    Randomly select text and colour of displayed text on button press
    Previously, TextScript::Guess() updated the text to the guess made.
    This commit randomly selects the new text from the list of possible options, and selects a random (different) colour for it.

--- Scripts/ButtonScript.cs
+++ Scripts/ButtonScript.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonScript : MonoBehaviour {
+public class ButtonScript : MonoBehaviour
+{
     public string txt;
     public Text target;
     Text textbox;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         textbox = transform.Find("Text").GetComponent<Text>();
         textbox.text = txt;
     }
 
     // Run from outside to change button text
-    public void ChangeText(string input) {
+    public void ChangeText(string input)
+    {
         txt = input;
         textbox.text = txt;
     }
 
     // Sets button text, run on click
-    public void Clicked() {
-        target.GetComponent<TextScript>().guess(txt);
+    public void Clicked()
+    {
+        target.GetComponent<TextScript>().Guess(txt);
     }
 }
--- Scripts/TextScript.cs
+++ Scripts/TextScript.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class ColourPair
+{
+    public string name;
+    public Color colour;
+
+    public ColourPair(string name_input, Color colour_input)
+    {
+        name = name_input;
+        colour = colour_input;
+    }
+}
+
 public class TextScript : MonoBehaviour
 {
+    List<ColourPair> colour_options = new List<ColourPair>();
+    Color green = Color.green;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         GetComponent<Text>().text = "new text";
+
+        colour_options.Add(new ColourPair("Black", Color.black));
+        colour_options.Add(new ColourPair("Blue", Color.blue));
+        colour_options.Add(new ColourPair("Red", Color.red));
+        colour_options.Add(new ColourPair("Green", Color.green));
+
+        Debug.Log(colour_options.Count);
     }
 
-    public void guess(string input) {
-        GetComponent<Text>().text = input;
+    // Given a "guess" from a button, regenerates the text.
+    public void Guess(string input) {
+
+        // By spec, two random ints cannot be the same
+        int name_index = Random.Range(0,4);
+        int colour_index;
+
+        do {
+            colour_index = Random.Range(0,4);
+        } 
+        while (colour_index == name_index);
+
+        GetComponent<Text>().text = colour_options[name_index].name;
+        GetComponent<Text>().color = colour_options[colour_index].colour;
+
+        Debug.Log($"Reroll: name {name_index}, colour {colour_index}");
     }
 }
