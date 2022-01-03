a7b33805d589ffe907e92f3ac589ff89e6a9d937
emmawithanE <emma@catgirl.software>
2022-01-03 22:09:17 +1030

    Refactor random selection of text and colour
    Shifting reroll code into TextScript::RerollColour() improves readability and allows reuse in Start() and guess().

--- Scripts/TextScript.cs
+++ Scripts/TextScript.cs
@@ -18,35 +18,39 @@ public class ColourPair
 public class TextScript : MonoBehaviour
 {
     List<ColourPair> colour_options = new List<ColourPair>();
-    Color green = Color.green;
+
+    string correct_guess = "";
+
+    void RerollColour()
+    {
+        int name_index = Random.Range(0, colour_options.Count);
+        int colour_index;
+
+        do
+        {
+            colour_index = Random.Range(0, colour_options.Count);
+        }
+        while (colour_index == name_index);
+
+        GetComponent<Text>().text = colour_options[name_index].name;
+        GetComponent<Text>().color = colour_options[colour_index].colour;
+        correct_guess = colour_options[colour_index].name;
+    }
 
     // Start is called before the first frame update
-    void Start() {
-        GetComponent<Text>().text = "new text";
-
+    void Start() 
+    {
         colour_options.Add(new ColourPair("Black", Color.black));
         colour_options.Add(new ColourPair("Blue", Color.blue));
         colour_options.Add(new ColourPair("Red", Color.red));
         colour_options.Add(new ColourPair("Green", Color.green));
 
-        Debug.Log(colour_options.Count);
+        RerollColour();
     }
 
     // Given a "guess" from a button, regenerates the text.
-    public void Guess(string input) {
-
-        // By spec, two random ints cannot be the same
-        int name_index = Random.Range(0,4);
-        int colour_index;
-
-        do {
-            colour_index = Random.Range(0,4);
-        } 
-        while (colour_index == name_index);
-
-        GetComponent<Text>().text = colour_options[name_index].name;
-        GetComponent<Text>().color = colour_options[colour_index].colour;
-
-        Debug.Log($"Reroll: name {name_index}, colour {colour_index}");
+    public void Guess(string input) 
+    {
+        RerollColour();
     }
 }
