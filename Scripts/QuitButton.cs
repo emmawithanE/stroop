8b38329d01b68fce33c2694b4b885f83f0706b03
emmawithanE <emma@catgirl.software>
2022-01-04 17:22:28 +1030

    Add a quit button

---
+++ Scripts/QuitButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    // Quit game when clicked
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
